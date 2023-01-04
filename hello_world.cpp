25e2dbffc9d61d2451f234cae487295b49cb4865
brisequentin <brise.quentin@gmail.com>
2023-01-04 17:11:33 +0100

    hello world

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World !" << std::endl;
+    return 0;
+}
+
+//compile 
+// g++ -std=c++17 -Wall -Werror hello_world.cpp -o helloWorld
+// ./helloworld
