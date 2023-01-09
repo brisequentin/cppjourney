8de4354c4735805fc57754ea0e6592a267d2142e
brisequentin <brise.quentin@gmail.com>
2023-01-09 20:56:06 +0100

    update

--- operation&condition.cpp
+++ operation&condition.cpp
@@ -2,6 +2,43 @@
 
 int main()
 {
+    int result;
+    // + - * / %
+    // ! && ||
+    // < <= > >= == !=
+    result = 5 + 5;
+    result++;
+
+    std::cout << "Resultat : " << result << std::endl;
+
+
+    // data = 0
+
+    //result = data++ // result = 0, data = 1
+    //result = ++data //result = 2, data = 2
+
+
+    if(true){
+        
+    }else if(false){
+        while(false){
+
+        }
+    }
+
+    if(int number{3};number>3){
+
+    }
+
+    switch(number){
+        case 1:
+        case 2:
+            std::cout << "slt" << std::endl;
+            break;
+        default:
+            break;
+
+    }
 
 
     return 0;
